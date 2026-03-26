a93000dfe3f2560970d10b31d20305406c24b1b7
Nehat876 <gundu8neha@gmail.com>
2026-03-26 21:35:13 +0530

    first commit

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ document.getElementById("feedbackForm").addEventListener("submit", function(even
     const emailValue = document.getElementById("femail").value;
     const messageValue = document.getElementById("fmsg").value;
 
-    fetch("https://divya-portfolio-project1.onrender.com") {
+    fetch("https://divya-portfolio-project1.onrender.com/contact") {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ name: nameValue, email: emailValue, message: messageValue })
